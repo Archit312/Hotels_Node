94018d10bace505e7ca9ac46eb5f54389e067cfb
Archit Agarwal <agarwalrupin1978@gmail.com>
2024-05-09 13:28:15 +0530

    Shift MongoDb to online server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
+const PORT = process.env.PORT || 3000;
 
 app.get('/',function(req,res){
     res.send('Welcome to my hotel.. How can i help u ?');
@@ -17,6 +19,7 @@ app.use('/person',personRoutes);
 app.use('/menu',menuRoutes);
 
 
-app.listen(3000,()=>{
+
+app.listen(PORT,()=>{
     console.log('Listening on port 3000');
 });
